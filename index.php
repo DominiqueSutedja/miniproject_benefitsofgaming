1a38fdaaeaf16b73f78dabe5b2ea1114b8521739
DominiqueSutedja <109221685+DominiqueSutedja@users.noreply.github.com>
2022-07-20 19:30:45 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    Welcome: <?php echo $_POST["fname"]; ?><?php echo $_POST["lname"]; ?>
+    <br>
+    Your submitted Email Adress is : <?php echo $_POST["email"]; ?>
+    <br>
+    Your submitted Age group is: <?php echo $_POST["age"];  ?>
+    <br>
+    Your submitted legth of gaming is: <?php echo $_POST["age of playing"];  ?>
+    <br>
+    Your submitted types of games is: <?php echo $_POST["interest"];  ?>
+    <!--
+    This script places a badge on your repl's full-browser view back to your repl's cover
+    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+    teal, blue, blurple, magenta, pink!
+    -->
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
